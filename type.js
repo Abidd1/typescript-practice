0aeb380ba82f1340e3950a322f929d7452d9067b
Abidd1 <abidali129418@gmail.com>
2025-07-28 08:12:52 -0700

    this is the typescript practice session

---
+++ type.js
@@ -0,0 +1,16 @@
+"use strict";
+// let persons: [string, number, boolean] = ['abid', 13, true]
+Object.defineProperty(exports, "__esModule", { value: true });
+var student1 = {
+    name: "abid ali web developer",
+    age: 23,
+    status: true,
+};
+var student2 = {
+    name: "ahmad",
+    age: 53,
+    status: false,
+};
+var allstudent = [student1, student2];
+;
+console.log(allstudent);
---
+++ type.ts
@@ -0,0 +1,37 @@
+
+// let persons: [string, number, boolean] = ['abid', 13, true]
+
+// export{}
+
+// let abbid: {name: string, rollNo: number, className: string} = {
+//     name: "abid",
+//     rollNo: 34,
+//     className: "WMA Batch H"
+// }
+
+type student = {
+    name: string,
+    age: number,
+    status: boolean,
+}
+
+let student1: student = {
+    name: "abid ali web developer",
+    age: 23,
+    status: true,
+}
+
+let student2 : student = {
+    name: "ahmad",
+    age: 53,
+    status: false,
+}
+
+let allstudent: student[] = [student1, student2];;
+
+console.log(allstudent);
+
+export{}
+
+
+
